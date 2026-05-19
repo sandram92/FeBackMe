7f185b133901ecdd1c8adf82dcf8a9076948bf04
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2026-05-19 15:11:25 +0100

    fix proxy

--- services/passport.js
+++ services/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       // Check if user already exists in our database
