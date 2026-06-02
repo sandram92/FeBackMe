b8ad78375e778da7ef2a43523b87bef25b5d65fd
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2026-06-02 16:47:57 +0100

    clean up secret keys for dev and prod

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,6 @@
 const passport = require("passport");
 
 module.exports = (app) => {
-  // console.log("authRoutes loaded", app);
   app.get(
     "/auth/google",
     passport.authenticate("google", {
